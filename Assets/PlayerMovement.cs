07fa4762436288b61bf102094d647b617f1b25b3
Yok Simlab <yoksimlab@gmail.com>
2024-01-25 20:39:36 +0200

    movegit add -A!

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -9,7 +9,14 @@ public class PlayMovement : MonoBehaviour
     public InputControls CustomInput = null;
     private float _moveVector;
     public float Speed = 5f;
-
+    private Rigidbody2D _rigidbody2D;
+    
+    void Awake()
+    {
+        CustomInput = new InputControls();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+    
     private void OnEnable()
     {
         CustomInput.Enable();
@@ -29,20 +36,14 @@ public class PlayMovement : MonoBehaviour
 
     private void Move(InputAction.CallbackContext callbackContext)
     {
-        _moveVector = callbackContext.ReadValue<Axis>();
-
-    }
-    
-
-    void FixedUpdate()
-    {
-        
-
+        _moveVector = callbackContext.ReadValue<float>();
     }
 
-    void Awake()
-    {
-        CustomInput = new InputControls();
 
+    private void Update()
+    {
+        float oldY = _rigidbody2D.velocity.y;
+        float x = _moveVector * Speed;
+        _rigidbody2D.velocity = new Vector2(x, oldY);
     }
 }
