cfd5bd8a9a8db43c3ade36250970209096f53183
OfekSusan <ofeksushantrainers@gmail.com>
2024-01-27 15:17:07 +0200

    Trampolinev2

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -86,6 +86,7 @@ public class PlayMovement : MonoBehaviour
             spriteSwitcher.isFrozen = true;
         }
 
+        gameObject.tag = "FrozenPlayer";
         isFrozen = true;
         globalGameMode.FreezeCounter++;
         _rigidbody2D.constraints = RigidbodyConstraints2D.FreezeAll;
@@ -167,8 +168,6 @@ public class PlayMovement : MonoBehaviour
 
     private void Jump()
     {
-        Debug.Log("isGrounded: " + isGrounded + " coyote timer: " + coyoteTimeCounter + " jumpbuffer timer " +
-                  jumpBufferTimeCounter);
         _rigidbody2D.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
 
         isJumping = true;
